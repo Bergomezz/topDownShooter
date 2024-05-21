303f2f2a8b588d892bb4c8c973599f702d5a2049
Bergomezz <bernardogomezz@yahoo.com.br>
2024-05-21 15:51:57 -0300

    feature: world setup and player set

--- main.lua
+++ main.lua
@@ -1,5 +1,19 @@
 function love.load()
+  score = 0
+  
 
+  sprites = {}
+  sprites.background = love.graphics.newImage("sprites/background.png")
+  sprites.bullet = love.graphics.newImage("sprites/bullet.png")
+  sprites.player = love.graphics.newImage("sprites/player.png")
+  sprites.zombie = love.graphics.newImage("sprites/zombie.png")
+
+  player = {}
+  player.x = love.graphics.getWidth()/2
+  player.y = love.graphics.getHeight()/2
+
+
+  gameFont = love.graphics.newFont(40)
 end
 
 
@@ -8,5 +22,8 @@ function love.update(dt)
 end
 
 function love.draw()
-
+  love.graphics.draw(sprites.background)
+  -- love.graphics.setFont(gameFont)
+  -- love.graphics.print("Score: " ..score, 5,5)
+  love.graphics.draw(sprites.player, player.x, player.y)
 end
