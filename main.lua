b69bbdee5490db4f5acc17ce52c70faa6b2aebda
Bergomezz <bernardogomezz@yahoo.com.br>
2024-05-22 13:18:45 -0300

    feature: enemy logic done

--- main.lua
+++ main.lua
@@ -13,7 +13,8 @@ function love.load()
   player.y = love.graphics.getHeight()/2
   player.speed = 180
 
-  tempRotation = 0
+  zombies = {}
+
   gameFont = love.graphics.newFont(40)
 end
 
@@ -32,7 +33,10 @@ function love.update(dt)
     player.y = player.y + player.speed*dt
   end
 
-  tempRotation = tempRotation + 0.01
+  for i,z in ipairs(zombies) do
+    z.x = z.x + (math.cos( zombiePlayerAngle(z) ) * z.speed * dt)
+    z.y = z.y + (math.sin( zombiePlayerAngle(z) ) * z.speed * dt)
+  end
 end
 
 function love.draw()
@@ -49,8 +53,40 @@ function love.draw()
     sprites.player:getWidth()/2, 
     sprites.player:getHeight()/2
   )
+
+  for i,z in ipairs(zombies) do
+    love.graphics.draw(
+      sprites.zombie, 
+      z.x, 
+      z.y, 
+      zombiePlayerAngle(z), 
+      nil, 
+      nil, 
+      sprites.zombie:getWidth()/2, 
+      sprites.zombie:getHeight()/2
+    )
+  end
+end
+
+function love.keypressed( key )
+  if key == "space" then
+    spawnZombie()
+  end
 end
 
 function playerMouseAngle()
   return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) + math.pi
+end
+
+function zombiePlayerAngle(enemy)
+  return math.atan2(player.y - enemy.y, player.x - enemy.x)
+end
+
+function spawnZombie()
+  local zombie = {}
+  zombie.x = math.random(0, love.graphics.getWidth())
+  zombie.y = math.random(0, love.graphics.getHeight())
+  zombie.speed = math.random(100, 180)
+
+  table.insert(zombies, zombie)
 end
